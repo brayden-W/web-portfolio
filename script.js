bff9da0070bfa64e61f4f1dbce46241816295c33
Brayden <bwickel@my.devry.edu>
2025-04-25 20:28:06 -0700

    Fix About me section Fix testimonials carousel

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-$('.gallery').flickity({
-    wrapAround: true,
-    autoPlay: 6000
+document.addEventListener('DOMContentLoaded', function () {
+  var flkty = new Flickity('.gallery', {
+    cellAlign: 'center', // Align cells to the center
+    contain: true,       // Keep cells within the container
+    wrapAround: true,    // Enable infinite scrolling
+    autoPlay: false,     // Disable autoplay
   });
+
+  // Trigger resize after the page has fully loaded
+  window.addEventListener('load', function () {
+    flkty.resize();
+  });
+});
